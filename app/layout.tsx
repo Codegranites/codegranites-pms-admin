a1211bd74fc48856a02460a334cc8c049573e358
Vxrcel <skyeparagon@gmail.com>
2023-12-01 13:20:40 +0100

    init

---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Work_Sans } from "next/font/google";
+import "./Styles/globals.scss";
+import Navbar from "@/app/Components/navs/Navbar";
+
+import GotoTop from "./Components/GotoTop";
+import StateContextProvider from "@/context/StateContext";
+
+const workSans = Work_Sans({
+	subsets: ["latin"],
+	display: "swap",
+	variable: "--font-work-sans",
+});
+
+export const metadata: Metadata = {
+	title: "Code Granites",
+	description: "Illuminating Ideas and Forging Solutions",
+};
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en" className={workSans.variable}>
+			<body className={workSans.className}>
+				<StateContextProvider>
+					<Navbar />
+
+					{children}
+
+					<GotoTop />
+				</StateContextProvider>
+			</body>
+		</html>
+	);
+}
