bca1b85e38ae7a4e0719b524d38ccefbb9b2d968
Vxrcel <skyeparagon@gmail.com>
2023-12-01 13:30:39 +0100

    config: Tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,36 +1,109 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+	darkMode: ["class"],
+	content: [
+		"./pages/**/*.{ts,tsx}",
+		"./components/**/*.{ts,tsx}",
+		"./app/**/*.{ts,tsx}",
+		"./src/**/*.{ts,tsx}",
 	],
-  theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
-    extend: {
-      keyframes: {
-        "accordion-down": {
-          from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
-    },
-  },
-  plugins: [require("tailwindcss-animate")],
-}
+	theme: {
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: {
+				"2xl": "1440px",
+			},
+		},
+		extend: {
+			colors: {
+				primary: "#052C42",
+				"primary-light": "#2E577D",
+				"primary-dark": "#031824",
+				"color-600": "#161616",
+				"color-normal": "#535353",
+				header: "#282828",
+				card: "#FBF9FA",
+				checked: "#2E577D",
+				test: "#2e5773",
+			},
+			keyframes: {
+				"accordion-down": {
+					from: { height: 0 },
+					to: { height: "var(--radix-accordion-content-height)" },
+				},
+				"accordion-up": {
+					from: { height: "var(--radix-accordion-content-height)" },
+					to: { height: 0 },
+				},
+				pulsing: {
+					"50%": {
+						opacity: "0.2",
+					},
+				},
+				loadspin: {
+					"100%": {
+						transform: "rotate(360deg)",
+					},
+				},
+				shimmer: {
+					"100%": {
+						transform: "translateX(100%)",
+					},
+				},
+				slideUp: {
+					"70%": {
+						opacity: "0.7",
+						transform: "translateY(50px)",
+					},
+					"100%": {
+						transform: "translateY(0)",
+						opacity: "1",
+					},
+				},
+				slideDown: {
+					"100%": {
+						transform: "translateY(0)",
+						opacity: "1",
+					},
+				},
+				slideNavUp: {
+					"100%": {
+						transform: "translateY(-112px)",
+						opacity: "0",
+					},
+				},
+				fadeOut: {
+					"50%": {
+						opacity: "0.7",
+					},
+					"100%": {
+						opacity: "1",
+					},
+				},
+				rotate3d: {
+					"0%": {
+						transform: "rotateY(0)",
+					},
+					"50%": { opacity: "0.5" },
+
+					"100%": {
+						transform: "rotateY(360deg)",
+					},
+				},
+			},
+			animation: {
+				"accordion-down": "accordion-down 0.2s ease-out",
+				"accordion-up": "accordion-up 0.2s ease-out",
+				shimmer: "shimmer 1.5s infinite",
+				slideUp: "slideUp 1s 0.2s ease forwards",
+				loadspin: "loadspin 1.2s linear infinite",
+				pulsing: "pulsing 1.5s ease infinite",
+				rotate3d: "rotate3d 2s 1s linear infinite",
+				slideDown: "slideDown 1s 0.2s ease forwards",
+				slideNavUp: "slideDown 1s 0.2s ease forwards",
+			},
+		},
+	},
+	plugins: [require("tailwindcss-animate")],
+};
