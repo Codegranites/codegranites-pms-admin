83bc332676f8106a7164964f7fd2f95a62fa8506
Vxrcel <skyeparagon@gmail.com>
2023-12-05 13:39:16 +0100

    Feat: basic setup for Moderator Dashboard

--- app/(Moderator-Dashboard)/layout.tsx
+++ app/(Moderator-Dashboard)/layout.tsx
@@ -1,18 +1,15 @@
-import GotoTop from '../../components/GotoTop';
-import AdminNavbar from '../../components/navs/AdminNavbar';
+import ModeratorNavbar from '../../components/navs/ModeratorNavbar';
 
 import SidebarMod from '../../components/sidebars/SidebarMod';
 
-export default function AdminLayout({ children }: { children: React.ReactNode }) {
+export default function ModeratorLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<>
 			<SidebarMod />
 			<section className="w-full relative pl-[96px] lg:pl-[270px]">
-				<AdminNavbar />
+				<ModeratorNavbar />
 				<div className="flex w-full pt-8 flex-col h-full relative lg:px-9">{children}</div>
 			</section>
-
-			<GotoTop />
 		</>
 	);
 }
