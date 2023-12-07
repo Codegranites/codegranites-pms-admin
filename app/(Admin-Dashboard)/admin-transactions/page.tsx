5a21a76f7d2263c7753e219e54c33558028c7bd0
Olumide Bambe <loyalsfc@gmail.com>
2023-12-07 16:51:23 +0100

    completed empty transaction state

--- app/(Admin-Dashboard)/admin-transactions/page.tsx
+++ app/(Admin-Dashboard)/admin-transactions/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import SuperAdminTransaction from '../../../components/super-admin-transaction/super-admin-transaction';
 
 const TransactionsAdmin = () => {
-	return <div>TransactionsAdmin</div>;
+	return (
+		<div>
+			<SuperAdminTransaction />
+		</div>
+	);
 };
 
 export default TransactionsAdmin;
