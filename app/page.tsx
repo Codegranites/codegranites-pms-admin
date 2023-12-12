1a8794e4aef9c94c057cf479ee2d0d4fd74ab47b
Vxrcel <skyeparagon@gmail.com>
2023-12-12 16:38:20 +0100

    chore: buttons

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,17 @@ import Link from 'next/link';
 
 export default function Home() {
 	return (
-		<main className="max-container w-full flex justify-between items-center pt-4">
+		<main className="max-container w-full flex justify-between items-center pt-4 px-4">
 			<Link
 				href="/admin-dashboard"
-				className="border border-primary rounded-xl px-4 py-2 text-primary hover:bg-primary hover:text-white transition-colors duration-700 hover:duration-300 font-medium"
+				className="border border-primary sm:rounded-xl px-4 py-2 text-primary hover:bg-primary hover:text-white transition-colors duration-700 hover:duration-300 text-xs sm:text-base sm:font-medium"
 			>
 				Admin Dashboard
 			</Link>
 			Hello World!
 			<Link
 				href="/dashboard"
-				className="border border-primary rounded-xl px-4 py-2 text-primary hover:bg-primary hover:text-white transition-colors duration-700 hover:duration-300 font-medium"
+				className="border border-primary sm:rounded-xl px-4 py-2 text-primary hover:bg-primary hover:text-white transition-colors duration-700 hover:duration-300 text-xs sm:text-base sm:font-medium"
 			>
 				Mod Dashboard
 			</Link>
