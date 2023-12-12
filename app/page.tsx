9658b75f5fa47721b80d86a236b78c735869d0d0
Vxrcel <skyeparagon@gmail.com>
2023-12-12 16:45:03 +0100

    chore: buttons

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,19 @@ import Link from 'next/link';
 
 export default function Home() {
 	return (
-		<main className="max-container w-full flex justify-between items-center pt-4 px-4">
+		<main className="max-container w-full flex justify-between items-center pt-4 px-4 relative">
 			<Link
 				href="/admin-dashboard"
-				className="border border-primary sm:rounded-xl px-4 py-2 text-primary hover:bg-primary hover:text-white transition-colors duration-700 hover:duration-300 text-xs sm:text-base sm:font-medium"
+				className="border border-primary sm:rounded-xl px-1 sm:px-4 py-1 sm:py-2 text-primary hover:bg-primary hover:text-white transition-colors duration-700 hover:duration-300 text-xs sm:text-base sm:font-medium"
 			>
 				Admin Dashboard
 			</Link>
-			Hello World!
+			<h1 className="-translate-y-[120px] absolute sm:relative transition-all duration-1000 sm:-translate-y-0 sm:inline sm:text-3xl md:text-5xl xl:text-7xl">
+				Hello World!
+			</h1>
 			<Link
 				href="/dashboard"
-				className="border border-primary sm:rounded-xl px-4 py-2 text-primary hover:bg-primary hover:text-white transition-colors duration-700 hover:duration-300 text-xs sm:text-base sm:font-medium"
+				className="border border-primary sm:rounded-xl px-1 sm:px-4 py-1 sm:py-2 text-primary hover:bg-primary hover:text-white transition-colors duration-700 hover:duration-300 text-xs sm:text-base sm:font-medium"
 			>
 				Mod Dashboard
 			</Link>
