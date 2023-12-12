0df92b9ff81016d26e2a89d0da82e53228aff523
David Abolade <69032300+A-believer@users.noreply.github.com>
2023-12-12 18:06:20 +0100

    feat: problem statement page ui complete

--- app/(Admin-Dashboard)/problem-statements/page.tsx
+++ app/(Admin-Dashboard)/problem-statements/page.tsx
@@ -4,12 +4,51 @@ import EmptyState from '../../../components/admin/problem-statements/EmptyState'
 import NewIdeaModal from '../../../components/admin/problem-statements/NewIdeaModal';
 import Problems from '../../../components/admin/problem-statements/Problems';
 
+export interface ProblemsDataProps {
+	id: number;
+	title: string;
+	description: string;
+	createdAt: string;
+}
+const currentDate: Date = new Date();
+
+const options: Intl.DateTimeFormatOptions = {
+	year: 'numeric',
+	month: 'short',
+	day: 'numeric',
+	hour: 'numeric',
+	minute: 'numeric',
+	hour12: true
+};
+
+const formattedDate = new Intl.DateTimeFormat('en-NG', options).format(currentDate);
+
+const problemData: ProblemsDataProps[] = [
+	{
+		id: 1,
+		title: 'John Doe Project',
+		description:
+			'I would like for the footer to be bigger and the land page to be more interesting add more pictures better colors smaller icons , styles like glass morphism would like for the footer to be bigger and the land page to be more interesting add more pictures better colors smaller icons , styles like glass morphism would like for the footer to be bigger and the land page to be more interesting add more pictures better colors smaller icons , styles like glass morphism would like for the footer to be bigger and the land page to be more interesting add more pictures better colors smaller icons , styles like glass morphism would like for the footer to be bigger and the land page to be more interesting add more pictures better colors smaller icons , styles like glass morphism would like for the footer to be bigger and the land page to be more interesting add more pictures better colors smaller icons , styles like glass morphism would like for the footer to be bigger and the land page to be more interesting add more pictures better colors smaller icons , styles like glass morphism would like for the footer to be bigger and the land page to be more interesting add more pictures better colors smaller icons , styles like glass morphism would like for the footer to be bigger and the land page to be more interesting add more pictures better colors smaller icons , styles like glass morphism',
+		createdAt: formattedDate
+	},
+	{
+		id: 2,
+		title: 'John Doe Project',
+		description:
+			'I would like for the footer to be bigger and the land page to be more interesting add more pictures better colors smaller icons , styles like glass morphism would like for the footer to be bigger and the land page to be more interesting add more pictures better colors smaller icons , styles like glass morphism would like for the footer to be bigger and the land page to be more interesting add more pictures better colors smaller icons , styles like glass morphism would like for the footer to be bigger and the land page to be more interesting add more pictures better colors smaller icons , styles like glass morphism would like for the footer to be bigger and the land page to be more interesting add more pictures better colors smaller icons , styles like glass morphism would like for the footer to be bigger and the land page to be more interesting add more pictures better colors smaller icons , styles like glass morphism would like for the footer to be bigger and the land page to be more interesting add more pictures better colors smaller icons , styles like glass morphism would like for the footer to be bigger and the land page to be more interesting add more pictures better colors smaller icons , styles like glass morphism would like for the footer to be bigger and the land page to be more interesting add more pictures better colors smaller icons , styles like glass morphism',
+		createdAt: formattedDate
+	}
+];
+
 const ProblemStatements: React.FC = () => {
 	const [isIdeaModal, setIsIdeaModal] = useState<boolean>(false);
 	return (
 		<div>
+			{problemData.length > 0 ? 
+			<Problems problemData={problemData} openNewIdeaModal={() => setIsIdeaModal(true)} />
+		:
 			<EmptyState openNewIdeaModal={() => setIsIdeaModal(true)} />
-			{/* <Problems openNewIdeaModal={() => setIsIdeaModal(true)} /> */}
+		}
 			{isIdeaModal && <NewIdeaModal isOpen={isIdeaModal} onClose={() => setIsIdeaModal(false)} />}
 		</div>
 	);
--- components/admin/problem-statements/Problems.tsx
+++ components/admin/problem-statements/Problems.tsx
@@ -1,27 +1,54 @@
 import Button from '@ui/Button';
-import { Add } from 'iconsax-react';
-import React from 'react';
+import { Add, ArrowRight2, MessageEdit, Trash } from 'iconsax-react';
 
-interface ProblemsDataProps {
-	id: number;
-	title: string;
-	description: string;
-	createdAt: Date;
+import React from 'react';
+import { ProblemsDataProps } from '../../../app/(Admin-Dashboard)/problem-statements/page';
+
+interface ProblemsProps {
+  openNewIdeaModal: () => void
+  problemData: ProblemsDataProps[]
 }
 
-const Problems: React.FC<{ openNewIdeaModal: () => void }> = ({ openNewIdeaModal }) => {
+const Problems: React.FC<ProblemsProps> = ({ openNewIdeaModal, problemData }) => {
 	return (
 		<div className="w-full flex flex-col">
 			<div className="w-fit self-end mr-12">
 				<Button
 					onClick={openNewIdeaModal}
 					leftIcon={<Add />}
-					className="text-sm leading-6 font-medium rounded-lg my-4 py-4 px-12"
+					className="text-sm leading-6 font-medium rounded-lg my-4 py-4 px-10"
 				>
 					New Idea
 				</Button>
 			</div>
-			<ul className="border-y w-full"></ul>
+			<ul className="border-t w-full">
+				{problemData && problemData?.map((problem) => (
+					<li
+						key={problem.id}
+						className="flex items-center justify-between pl-6 pr-12 py-4 border-b text-sm leading-6 font-medium"
+					>
+						<div className="flex items-center gap-x-2">
+							{<ArrowRight2 />}
+							<p>{problem.title}</p>
+							<p className="font-normal">
+								{problem.description.length > 40 ? problem.description.slice(0, 50) + '......' : problem.description}
+							</p>
+						</div>
+						<div>{problem.createdAt}</div>
+
+						<div className="flex items-center gap-x-3">
+							<button className="flex items-center gap-x-2 text-primary-light">
+								<MessageEdit />
+								<span>Edit</span>
+							</button>
+							<button className="flex items-center gap-x-2 text-[#FF3333]">
+								<Trash fill={'#FF3333'} />
+								<span>Delete</span>
+							</button>
+						</div>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 };
