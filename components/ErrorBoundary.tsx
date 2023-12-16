7458da0c01479912fe772ebdb47410768652cf84
James Ayomikun Araoye <ayomikuntemitope.araoye@gmail.com>
2023-12-16 11:55:41 +0100

    Update components

---
+++ components/ErrorBoundary.tsx
@@ -0,0 +1,58 @@
+
+import React, { Component, ErrorInfo, ReactNode } from 'react';
+
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+  onLogError?: (error: Error, errorInfo: ErrorInfo) => void;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(_: Error): ErrorBoundaryState {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+    console.error('Error caught by ErrorBoundary:', error, errorInfo);
+
+    if (this.props.onLogError) {
+      this.props.onLogError(error, errorInfo);
+    }
+    // You can log the error to a server or external service here
+
+    // Optionally, you can perform additional actions based on the error
+  }
+
+  handleTryAgainClick = (): void => {
+    this.setState({ hasError: false });
+  };
+
+  render(): ReactNode {
+    if (this.state.hasError) {
+      return (
+        <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+          <p className="font-bold">Something went wrong.</p>
+          <button
+            onClick={this.handleTryAgainClick}
+            className="bg-primary-100 hover:bg-secondary-100 text-white font-bold py-2 px-4 rounded mt-2"
+          >
+            Try Again
+          </button>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
