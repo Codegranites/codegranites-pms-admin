b1d326e4561fb90b470e3581bbc3555d818ca67b
Vxrcel <skyeparagon@gmail.com>
2024-01-03 12:16:10 +0100

    Feat: splash screen

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,25 @@ import Footer from '../components/footer/Footer';
 import Hero from '../components/home/hero';
 import MIdsec from '../components/home/midsec';
 import Feats from '../components/home/feat';
+import SplashScreen from './SplashScreen';
+import { useStateCtx } from '../context/StateContext';
 export default function Home() {
+	const { pageLoaded } = useStateCtx();
+
 	return (
 		<>
-			<Navbar />
-			<Hero />
-			<main className="max-container w-full flex flex-col min-h-screen">
-				<MIdsec />
-				<Feats />
-			</main>
-			<Footer />
+			<SplashScreen />
+			{pageLoaded && (
+				<>
+					<Navbar />
+					<Hero />
+					<main className="max-container w-full flex flex-col min-h-screen">
+						<MIdsec />
+						<Feats />
+					</main>
+					<Footer />
+				</>
+			)}
 		</>
 	);
 }
