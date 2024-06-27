fe0a7103c734ce5b38b31fcc634e3d0a81a243a8
hisreal123 <omosgoerge@gmail.com>
2024-06-27 14:37:19 -0500

    LOGO: fix image

--- components/forms/SignUpForm.tsx
+++ components/forms/SignUpForm.tsx
@@ -64,7 +64,7 @@ const SignUpForm = () => {
 
   return (
     <div className="relative  px-4 sm:px-6 z-20 w-full mx-auto  overflow-y-scroll pb-4 mt-8">
-      <div className="wrapper_auth_top relative pt-20 ">
+      <div className="wrapper_auth_top relative pt-18 ">
         <Link href="/" className="logo w-[100px] block">
           <Image
             src="/logo.png"
